3dcf039e6ca9a90c9f934f4da374d4b9d7b4134c
Coding Forest <64945652+Coding-Forest@users.noreply.github.com>
2021-11-26 04:58:32 +1100

    Update h-index.py

--- 03 Challenges/Programmers/h-index.py
+++ 03 Challenges/Programmers/h-index.py
@@ -1,3 +1,4 @@
+# solution 1
 import numpy as np
 
 def solution(citations):
@@ -12,4 +13,18 @@ def solution(citations):
 
     return answer
 
-solution([3, 0, 6, 1, 5])
+
+# solution 2
+def solution(citations):
+    
+    total = len(citations)
+    citations.sort()
+    answer = 0
+
+    for i in range(total):
+        h = total - i
+        if (citations[i] >= h):
+            answer = h
+            break
+
+    return answer
